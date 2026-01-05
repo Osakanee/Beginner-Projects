002232b5deec788e83d844f9cafe3ee8341ccd7c
Osaka <bfdiwdr@gmail.com>
2026-01-05 14:40:48 -0300

    New punishment and exit system

--- counting.py
+++ counting.py
@@ -3,13 +3,18 @@ from rich import print
 def counting():
     global currentnumber
     currentnumber = 0
+    wrongtries = 0
     while True:
         count = int(input("> "))
         if count == currentnumber + 1:
             print(currentnumber + 2)
             currentnumber += 2
+        elif wrongtries == 2:
+            print("You think you're soooo funny huh.")
+            break
         else:
             print("[red]Wrong number![/red]")
+            wrongtries += 1
 
 print("[yellow]Welcome to counting.py![/yellow]")
 print("Start [blue]counting[/blue], and i'll go with you!")

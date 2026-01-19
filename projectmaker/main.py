6c59b6d3bb49f87328bc90e8f52de10d15aebf3a
Osaka <bfdiwdr@gmail.com>
2026-01-19 17:50:23 -0300

    It works

---
+++ projectmaker/main.py
@@ -0,0 +1,34 @@
+import os 
+from pathlib import Path
+
+bannedlist = ["C:/System32", "C:/System", "C:/Program Files", "C:/Program Files (x86)", "C:/Windows"]
+
+def starterkit(path):
+    path.mkdir(parents=True, exist_ok=True)
+    src = path / "src"
+    assets = path / "assets"
+    readme = path / "README.md"
+    requirements = path / "requirements.txt"
+    src.mkdir()
+    assets.mkdir()
+    readme.touch()
+    requirements.touch()
+
+while True:
+    print("〃 Gimme an absolute path to make the folder and the subfolders 〃")
+    thestuff = input("> ")
+    p = Path(thestuff)
+    if p in bannedlist:
+        print("Not possible to continue your request.")
+        break
+    elif thestuff == "exit":
+        break
+    if p.is_dir() == False:
+        starterkit(p)
+    else:
+        print("Error, a folder already exists with that name or this isn't an absolute path")
+        print("example of absolute path: C:/Users/YourUser/Downloads/YourProject")
+
+
+
+
